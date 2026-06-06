ff9416480499b0c76569f7eb6fa0ab89948a2188
ArZon <emonsitto27@gmail.com>
2026-06-05 21:59:40 -0500

    Create Temperature&Humidity_Sensor.ino

---
+++ Temperature and HumiditySensor/Source/Temperature&Humidity_Sensor.ino
@@ -0,0 +1,48 @@
+
+// date: 6/5/2026
+// Temperature and HumiditySensor by: Armanz0n
+
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+void setup( )
+{
+}
+
+static bool measure_environment( float *temperature, float *humidity ) // Returns true when time interval lapses and updates temp/humidity via pointers
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
+
+void loop( )
+{
+  float temperature; //initialize the variable Temperature
+  float humidity; //initialize the variable Humidity
+
+  // Measure temperature and humidity.  If the functions returns true, then a measurement is available. 
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "Temperature = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " Celsius, Humidity = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
+}
+
+//code inspired from the elegoo super starter kit
